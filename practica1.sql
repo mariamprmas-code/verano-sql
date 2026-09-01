2c37a1713900ce045cdf2bdf04522c6062b83c0b
marian <mariamprmas@gmail.com>
2026-09-01 16:36:39 +0200

    sigo práctica

--- practica1.sql
+++ practica1.sql
@@ -17,5 +17,24 @@ VALUES ('Bases de Datos' , 'Rafa' , 160);
 -- 5. Consultamos los datos que acabamos de guardar
 SELECT * FROM asignaturas;
 SELECT * FROM asignaturas WHERE horas > 100;
+-- Añadir una nueva columna llamada 'aula'
+ALTER TABLE asignaturas ADD COLUMN aula VARCHAR(20);
+-- Insertar nuevos datos con la nueva columna
+INSERT INTO asignaturas  (nombre, profesor, horas, aula)
+VALUES ('Programación', 'Carlos', 180, 'Lab 1');
+-- Consultar toda la tabla actualizada para comprobarlo
+SELECT * FROM asignaturas; 
+
+-- 1. Consulta básica para ver todo
+
+SELECT * FROM asignaturas;
+
+-- 2. Consulta con filtro de numeros,  por ej. ver solo las asignaturas que superan las 100 horas
+
+SELECT * FROM asignaturas WHERE horas > 100;
+
+-- 3. Consulta especifica : ver solo el nombre y profesor de las asignaturas largas
+
+SELECT nombre, profesor FROM asignaturas WHERE horas > 100;
 
 
