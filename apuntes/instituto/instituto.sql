459c7e52beb9de3732ae8bb5e8d85a92c79c34b5
marian <mariamprmas@gmail.com>
2026-09-15 18:21:06 +0200

    comandos JOIN

--- apuntes/instituto/instituto.sql
+++ apuntes/instituto/instituto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-09-2026 a las 07:23:52
+-- Tiempo de generación: 15-09-2026 a las 17:36:53
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `instituto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `edad`) VALUES
+(1, 'Ana', 'García', 20),
+(2, 'Carlos', 'López', 22);
 
 -- --------------------------------------------------------
 
@@ -47,9 +68,36 @@ INSERT INTO `asignaturas` (`id`, `nombre`, `profesor`, `horas`, `aula`) VALUES
 (6, 'Entornos de Desarrollo', 'Pedro', 90, '9'),
 (7, 'Redes', 'Ana', 120, '3');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matriculas`
+--
+
+CREATE TABLE `matriculas` (
+  `id` int(11) NOT NULL,
+  `alumno_id` int(11) DEFAULT NULL,
+  `asignatura_id` int(11) DEFAULT NULL,
+  `calificacion` decimal(4,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `matriculas`
+--
+
+INSERT INTO `matriculas` (`id`, `alumno_id`, `asignatura_id`, `calificacion`) VALUES
+(1, 1, 2, 9),
+(2, 2, 3, 7);
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `asignaturas`
@@ -58,14 +106,32 @@ ALTER TABLE `asignaturas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `asignaturas`
 --
 ALTER TABLE `asignaturas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
