53c3b207e8e4ca35ee2edf374ac97d3ea030a5f1
mariamprmas-code <mariamprmas@gmail.com>
2026-09-15 15:16:50 +0200

    Create database and manage asignaturas table
    Added SQL script to create a database and manage subjects.

---
+++ apuntes/practicas.sql
@@ -0,0 +1,55 @@
+-- 1. Creamos la base de datos para las prácticas 
+CREATE DATABASE instituto;
+-- 2. Le decimos a MySQL que use esta base de datos
+USE instituto;
+-- 3. Creamos nuestra primera tabla de asignaturas
+CREATE TABLE asignaturas (
+id INT AUTO_INCREMENT PRIMARY KEY ,
+nombre VARCHAR(50) ,
+profesor VARCHAR(50) ,
+horas INT
+) ;
+-- 4. Insertamos nuestros primeros datos
+INSERT INTO asignaturas (nombre, profesor, horas)
+VALUES ('Lenguajes de Marcas', 'Sonia', 96);
+INSERT INTO asignaturas (nombre, profesor, horas)
+VALUES ('Bases de Datos' , 'Rafa' , 160);
+-- 5. Consultamos los datos que acabamos de guardar
+SELECT * FROM asignaturas;
+SELECT * FROM asignaturas WHERE horas > 100;
+-- Añadir una nueva columna llamada 'aula'
+ALTER TABLE asignaturas ADD COLUMN aula VARCHAR(20);
+-- Insertar nuevos datos con la nueva columna
+INSERT INTO asignaturas  (nombre, profesor, horas, aula)
+VALUES ('Programación', 'Carlos', 180, 'Lab 1');
+-- Consultar toda la tabla actualizada para comprobarlo
+SELECT * FROM asignaturas; 
+
+-- 1. Consulta básica para ver todo
+
+SELECT * FROM asignaturas;
+
+-- 2. Consulta con filtro de numeros,  por ej. ver solo las asignaturas que superan las 100 horas
+
+SELECT * FROM asignaturas WHERE horas > 100;
+
+-- 3. Consulta especifica : ver solo el nombre y profesor de las asignaturas largas
+
+SELECT nombre, profesor FROM asignaturas WHERE horas > 100;
+
+-- 4. Consulta ordenada de mayor a menor numero de horas
+
+SELECT * FROM asignaturas ORDER BY horas DESC;
+
+-- 5. Consulta con doble condición (mas de 100 horas y de un profesor concreto
+
+SELECT * FROM asignaturas where horas > 100 AND profesor = 'Rafa';
+
+-- 6. Modificar un dato existente (actualizar el aula de una asignatura)
+
+UPDATE asignaturas SET aula = 'Lab 2' WHERE nombre = 'Bases de Datos';
+
+-- Comprobamos que el cambio se ha guardado bien
+
+SELECT * FROM asignaturas;
+
